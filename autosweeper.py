cb944c86d5d500775fa2a90d82431627912f8523
ProDreVil <leejamesbenigno59@gmail.com>
2026-06-23 13:08:02 +0800

    Added main file and resources

---
+++ autosweeper.py
@@ -0,0 +1,59 @@
+# TODO: Automated Minesweeper
+# Color/Number Detection
+# 3 Modes: Flagging, Revealing, Chording
+# Download Selenium
+# Corners, 1 2 3, 121 Combo
+
+import pyautogui, time
+
+def zzz():
+    time.sleep(0.5)
+
+def openBrowser():
+    browser = pyautogui.locateCenterOnScreen('chrome.png', confidence = 0.8)
+    if browser is not None:
+        pyautogui.click(browser)
+        pyautogui.hotkey('ctrl', 't')
+    else:
+        print("Chrome icon not found")
+    zzz()
+
+def openMinesweeper():
+    mso = None
+    while mso is None:
+        mso = pyautogui.locateCenterOnScreen('minesweeperonline.png', confidence = 0.8)
+        pyautogui.click(mso)
+        zzz()
+    zzz()
+
+def difficulty():
+    diff = "beginner" # u can change the difficulty here lol (beginner, intermediate, expert)
+    img = f"{diff}.png"
+    pos = None
+    while pos is None:
+        pos = pyautogui.locateCenterOnScreen(img, confidence = 0.8)
+        if pos is not None:
+            pyautogui.click(pos)
+        else:
+            print("Difficulty button not found")
+        zzz()
+    zzz()
+
+zzz()
+openBrowser()
+openMinesweeper()
+difficulty()
+
+# from selenium import webdriver
+
+# driver = webdriver.Chrome()
+# driver.get("https://roblox.com")
+
+# pyautogui.PAUSE = 0.3
+
+# for i in range(10):
+#     print(pyautogui.position())
+#     time.sleep(0.3)
+
+#pyautogui.moveTo(100, 100)
+#pyautogui.click()
